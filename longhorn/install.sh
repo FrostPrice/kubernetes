9ec49c4691fd7858da259b12b85f9d084b44182d
Mateus Barbosa <barbosamat16@gmail.com>
2023-09-04 11:04:28 -0300

    Feat: Add more configuration to longhorn helm

--- longhorn/install.sh
+++ longhorn/install.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+# Variables
+DIRECTORY_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P ) # This will get the current directory absolute path
+
 # helm repo add longhorn https://charts.longhorn.io
 # helm repo update
 
 # For generic Kubernetes
-helm install longhorn longhorn/longhorn --namespace longhorn-system --create-namespace --version 1.5.1
+helm install longhorn longhorn/longhorn --namespace longhorn-system --create-namespace --version 1.5.1 --values $DIRECTORY_PATH/values.yaml
 
 # Check that Longhorn is running
 kubectl -n longhorn-system get pod
