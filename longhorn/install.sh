a3255afa49607b9d624fcaf9e093e1d9549ac454
Mateus Barbosa <barbosamat16@gmail.com>
2023-09-04 11:20:27 -0300

    Feat: Add more longhorn automated configs

--- longhorn/install.sh
+++ longhorn/install.sh
@@ -1,7 +1,25 @@
 #!/bin/sh
 
-# Variables
 DIRECTORY_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P ) # This will get the current directory absolute path
+
+# Used to create a new random user credentials
+DEFAULT_USER=$(cat $DIRECTORY_PATH/.DEFAULT_USER 2>/dev/null)
+DEFAULT_PASSWORD=$(cat $DIRECTORY_PATH/.DEFAULT_PASSWORD 2>/dev/null)
+
+if [ -z "$DEFAULT_USER" ]; then
+  DEFAULT_USER=$(
+    echo $RANDOM | md5sum | head -c 20
+    echo
+  )
+  echo $DEFAULT_USER >$DIRECTORY_PATH/.DEFAULT_USER
+fi
+if [ -z "$DEFAULT_PASSWORD" ]; then
+  DEFAULT_PASSWORD=$(
+    echo $RANDOM | md5sum | head -c 20
+    echo
+  )
+  echo $DEFAULT_PASSWORD >$DIRECTORY_PATH/.DEFAULT_PASSWORD
+fi
 
 # helm repo add longhorn https://charts.longhorn.io
 # helm repo update
@@ -13,5 +31,18 @@ helm install longhorn longhorn/longhorn --namespace longhorn-system --create-nam
 kubectl -n longhorn-system get pod
 
 # For authentication and UI access
-# USER=<USERNAME_HERE>; PASSWORD=<PASSWORD_HERE>; echo "${USER}:$(openssl passwd -stdin -apr1 <<< ${PASSWORD})" >> ./auth
-# kubectl -n longhorn-system create secret generic basic-auth --from-file=auth
+USER=$DEFAULT_USER; PASSWORD=$DEFAULT_PASSWORD; echo "${USER}:$(openssl passwd -stdin -apr1 <<< ${PASSWORD})" >> auth
+kubectl -n longhorn-system create secret generic basic-auth --from-file=auth
+
+echo ///////////////////////////////////////////////////////
+echo 
+echo Credential for the newly created User
+echo Username: $(cat $DIRECTORY_PATH/.DEFAULT_USER)
+echo Password: $(cat $DIRECTORY_PATH/.DEFAULT_PASSWORD)
+echo 
+echo ///////////////////////////////////////////////////////
+
+unset DEFAULT_USER
+unset DEFAULT_PASSWORD
+unset USER
+unset PASSWORD
